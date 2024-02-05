e4b5129358759a1bd9ad379879921105bfe5834a
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2024-02-05 22:50:58 +0530

    work pls

--- website/src/pages/api/Update.ts
+++ website/src/pages/api/Update.ts
@@ -1,7 +1,7 @@
 
 import type { APIRoute } from 'astro';
 // import {getAllUsers} from "../../lib/db";
-import {AddOrUpdateData } from "../../lib/db";
+import { AddOrUpdateData } from "../../lib/db";
 
 // Not needed right now but could be used in the future
 // export const GET: APIRoute = async ({ params, request }) => {
@@ -25,7 +25,11 @@ export const POST: APIRoute = async ({ request }) => {
         const User = await request.json();
         // console.log(User.username, User)
         await AddOrUpdateData(User.username, User);
-        return new Response(null, { status: 200 });
+        return new Response(null, {
+            status: 200, headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
     }
     return new Response(null, { status: 400 });
 }
